59341c20f82e8e8c9307ec21407d86d8da32c62b
Mitchell <Braden.Mitchell@gov.bc.ca>
2022-10-14 08:02:47 -0700

    Added swagger and reworked docker setup

--- backend/dist/health/health.module.js
+++ backend/dist/health/health.module.js
@@ -6,15 +6,15 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.AppService = void 0;
+exports.HealthModule = void 0;
 const common_1 = require("@nestjs/common");
-let AppService = class AppService {
-    getServer() {
-        return 'API Running!';
-    }
+const health_controller_1 = require("./health.controller");
+let HealthModule = class HealthModule {
 };
-AppService = __decorate([
-    (0, common_1.Injectable)()
-], AppService);
-exports.AppService = AppService;
-//# sourceMappingURL=app.service.js.map
+HealthModule = __decorate([
+    (0, common_1.Module)({
+        controllers: [health_controller_1.HealthController],
+    })
+], HealthModule);
+exports.HealthModule = HealthModule;
+//# sourceMappingURL=health.module.js.map
