979dbe0f20ad86f7ea8d4dd7a397d441c46971bf
Adam Kroon <Adam.Kroon@gov.bc.ca>
2022-10-24 20:19:14 -0700

    Update deploy.sh

--- .github/helpers/deploy.sh
+++ .github/helpers/deploy.sh
@@ -6,7 +6,7 @@ objects=( $DC_TEMPLATE $SERVICE_TEMPLATE $ROUTE_TEMPLATE )
 
 for i in "${objects[@]}"
 do
-    oc process -f /home/runner/work/cirmo-dpia/cirmo-dpia/openshift/templates/$DIR/"$i" --namespace=$NAMESPACE \
+    oc process -f /home/runner/work/citz-imb-build-with-us-1/citz-imb-build-with-us-1/openshift/templates/$DIR/"$i" --namespace=$NAMESPACE \
         -p APPLICATION_NAME=$APPLICATION_NAME \
         -p LICENSE_PLATE=$LICENSE_PLATE \
         -p ENVIRONMENT=$ENVIRONMENT \
