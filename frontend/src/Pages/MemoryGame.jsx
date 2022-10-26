3543504e090902b172a7dafadfe94cdf6467677d
SDToews <scott.toews@gov.bc.ca>
2022-10-26 15:21:03 -0700

    added grid

--- frontend/src/Pages/MemoryGame.jsx
+++ frontend/src/Pages/MemoryGame.jsx
@@ -1,16 +1,28 @@
+import { Grid } from '@mui/material'
+import styled from '@mui/system/styled'
 import React from 'react'
-import { MUIBox } from '../Layout/MUI/Box/MUI-Box'
-import { MUIContainer } from '../Layout/MUI/Box/MUI-Container'
 import Data from '../mockdata/memorygame.json'
 
+const Item = styled('div')(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    border: '1px solid',
+    borderColor: theme.palette.mode === 'dark' ? '#444d58' : '#ced7e0',
+    padding: theme.spacing(1),
+    borderRadius: '4px',
+    textAlign: 'center',
+}));
+
+
 export const MemoryGame = () => {
-  return (
-       <MUIContainer>
-            {
-                Data.map(item => {
-                    return <MUIBox key={item.id}>{item.name}</MUIBox>
-                })
-            }
-       </MUIContainer>
-  )
+    return (
+        <Grid container spacing={2}>
+            <Grid xs={3}>
+                {
+                    Data.map(item => {
+                        return <Item key={item.id}>{item.name}</Item>
+                    })
+                }
+            </Grid>
+        </Grid>
+    )
 }
