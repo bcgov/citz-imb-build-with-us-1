41014524e8cd0a510ae14ed5992d8b643d42c46f
Brady Mitchell <braden.jr.mitch@gmail.com>
2022-12-05 08:07:10 -0800

    Added fake user data to the get user endpoint

--- matchmaking/backend/controllers/users.js
+++ matchmaking/backend/controllers/users.js
@@ -1,7 +1,30 @@
+const fakeUsers = [
+  {
+    guid: 1001,
+    mentor_guid: 1002,
+    groud_id: 405,
+    points: 500,
+  },
+  {
+    guid: 1002,
+    mentor_guid: 1003,
+    groud_id: 305,
+    points: 950,
+  }
+];
+
 /**
  * Get user by id.
  * @method GET
  */
 exports.get_user = (req, res) => {
-  res.send('NOT IMPLEMENTED: GET User by id');
+  const user = fakeUsers.filter(user => {
+    return user.guid == req.params.id
+  })[0];
+
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(404).send('User not found.');
+  };
 };
