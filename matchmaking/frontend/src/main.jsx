e4d8d79f9bf8ee728030cc076f9b8c0bf867fa5b
Zach Bourque <113370162+ZachBourqueBCGov@users.noreply.github.com>
2023-01-06 14:15:00 -0800

    Landing Page (#41)
    * Added landing page without keycloak.
    
    * Update matchmaking/frontend/src/theme.js
    
    BCSans is one word.
    
    * Update index.html
    
    Co-authored-by: Brandon Bouchard <58348396+Bottle7@users.noreply.github.com>

--- matchmaking/frontend/src/main.jsx
+++ matchmaking/frontend/src/main.jsx
@@ -7,7 +7,8 @@ import Header from "./layout/Header";
 import Footer from "./layout/Footer";
 import { ThemeProvider } from "@mui/material";
 import theme from "./theme";
-import './css/common.css';
+import "./css/common.css";
+import LandingPage from "./pages/LandingPage";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -15,7 +16,8 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <Router>
         <Header />
         <Routes>
-          <Route path="/" element={<MemberList />} />
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/members" element={<MemberList />} />
           <Route path="/memory" element={<MemoryGame />} />
         </Routes>
         <Footer />
