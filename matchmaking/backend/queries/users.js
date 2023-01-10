829907b9d750c62d10f9e9cab9c5ed6e634e920c
Brady Mitchell <braden.jr.mitch@gmail.com>
2023-01-10 11:42:38 -0800

    Nodemon working, and api database queries working

--- matchmaking/backend/queries/users.js
+++ matchmaking/backend/queries/users.js
@@ -5,20 +5,20 @@ const db = require('../db-connection');
  * @method GET
  * @returns all rows from user table.
  */
-exports.getUsers = async () => await db.query('SELECT * FROM user');
+exports.getUsers = async () => await db.query('SELECT * FROM "user"');
 
 /**
  * Get user by id.
  * @method GET
  * @returns first row matching specified id to guid column in user table.
  */
-exports.getUserById = async (id) => await db.query('SELECT * FROM user WHERE guid = $id', [id]);
+exports.getUserById = async (id) => await db.query('SELECT * FROM "user" WHERE guid = $1', [id]);
 
 /**
  * Create user.
  * @method POST
  * @returns row of inserted user.
  */
-exports.createUser = async (guid, group_id, job_role) => 
-  await db.query('INSERT INTO user (guid, group_id, job_role) VALUES ($guid, $group_id, $job_role)', 
+exports.createUser = async (guid, group_id, job_role) =>
+  await db.query('INSERT INTO "user" (guid, group_id, job_role) VALUES ($1, $2, $3)', 
     [guid, group_id, job_role]);
