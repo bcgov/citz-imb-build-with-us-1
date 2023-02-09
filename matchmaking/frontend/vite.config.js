e5e4752627082a7006f3d75e7c245a4998d64d27
Zachary Bourque <Zachary.Bourque@gov.bc.ca>
2023-02-09 10:26:47 -0800

    Removed unused code in vite.config.js. Removed unused envvars in vite.config.js

--- matchmaking/frontend/vite.config.js
+++ matchmaking/frontend/vite.config.js
@@ -1,9 +1,5 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import dotenv from "dotenv";
-dotenv.config({ path: `${__dirname}/../../.env` });
-
-process.env = { ...process.env, ...dotenv.config().parsed };
 
 export default ({ command, mode }) => {
   return defineConfig({
